3f85fba6abe4eea1061aea2d6c95b33c1576ca4f
GerardoHasbum <ghasbumgo@gmail.com>
2024-03-01 14:15:46 -0600

    Clases y GUI creado sin codigo

---
+++ src/lab7p2_gerardohasbum/Producto.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7p2_gerardohasbum;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author ghasb
+ */
+public class Producto {
+    
+    private int id, category, bin, aisle;
+    private String precio, nombre;
+
+    public Producto() {
+    }
+
+    public Producto(int id, int category, int bin, int aisle, String precio, String nombre) {
+        this.id = id;
+        this.category = category;
+        this.bin = bin;
+        this.aisle = aisle;
+        this.precio = precio;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getBin() {
+        return bin;
+    }
+
+    public void setBin(int bin) {
+        this.bin = bin;
+    }
+
+    public int getAisle() {
+        return aisle;
+    }
+
+    public void setAisle(int aisle) {
+        this.aisle = aisle;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + id + "/n Category: " + category + "\n Bin: " + bin + "\n Aisle: " + aisle + "\n Precio: " + precio;
+    }
+    
+}
